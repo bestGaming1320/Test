f58409fd50b5a4c5df81a6b16972f0c1552bfb8c
bestGaming1320 <59489532+bestGaming1320@users.noreply.github.com>
2021-09-06 18:53:18 +0200

    c

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const editf = require('edit-file');
 
 try {
   // `who-to-greet` input defined in action metadata file

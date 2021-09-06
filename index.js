dd98280d6018ee99665b838e33d6b59697d75f9c
bestGaming1320 <59489532+bestGaming1320@users.noreply.github.com>
2021-09-06 19:59:06 +0200

    c

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const github = require('@actions/github');
 const editf = require('edit-file');
 
 try {
+  const fileN = core.getInput('fileN'); 
+	/*
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
   console.log(`Hello ${nameToGreet}!`);
@@ -10,7 +12,8 @@ try {
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  console.log(`The event payload: ${payload}`);*/
+  editf(fileN, (new Date()).toTimeString());
 } catch (error) {
   core.setFailed(error.message);
 }
